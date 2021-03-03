8ca31c37645c5cc5abb3d58d58f0e64623ba5d97
CaroKris <ckhilpisch@gmail.com>
2021-03-03 16:37:34 -0600

    Removing Develop folder

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
     fs.readFile(notesDatapath, "utf-8", function (err, data) {
       if (err) {
-        console.log(err);
+        res.status(500).send({error : "couldn't read saved notes"});
       } else {
         let savedNotes = JSON.parse(data);
         res.json(savedNotes);
@@ -20,7 +20,7 @@ module.exports = function (app) {
     
     fs.readFile(notesDatapath, "utf-8", function (err, data) {
       if (err) {
-        console.log(err);
+        res.status(500).send({error : "couldn't read saved notes"});
       } 
       let savedNotes = [];
       if (data) {
@@ -30,38 +30,38 @@ module.exports = function (app) {
       let newNote = req.body;
       newNote.id = uuidv4();
 
-      if (savedNotes) {
-        savedNotes.push(newNote);
-      } else {
-        savedNotes = [newNote];
-      }
+      // if (savedNotes) {
+      savedNotes.push(newNote);
+      // } else {
+      //   savedNotes = [newNote];
+      // }
 
       fs.writeFile(notesDatapath, JSON.stringify(savedNotes), "utf-8", (err) => {
         if (err) {
-          console.log(err);
+          res.status(500).send({error : "couldn't add new note"});;
         } else {
           console.log(savedNotes);
+          res.json(savedNotes);
         }
-      });
-      res.json(savedNotes);
+      });  
     });
   });
 
   app.delete("/api/notes/:id", function (req, res) {
     fs.readFile(notesDatapath, "utf-8", (err, data) => {
       if (err) {
-        console.log(err);
+        res.status(500).send({error : "couldn't delete note"});  
       } else {
         const newData = JSON.parse(data);
         const deleted = req.params.id;
         console.log(data);
-        notes = newData.filter((data) => data.id != deleted);
+        let notes = newData.filter((data) => data.id != deleted);
         fs.writeFile(notesDatapath, JSON.stringify(notes), "utf-8", (err) => {
           if (err) {
-            console.log(err);
+            res.status(500).send({error : "couldn't write after deleted note"});
           }
-        });
-        res.json(notes);
+          res.json(notes);
+        });  
       }
     });
   });
