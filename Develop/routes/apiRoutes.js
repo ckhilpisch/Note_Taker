9021bfc6277975c9fd2836e1f16998f47613f22e
CaroKris <ckhilpisch@gmail.com>
2021-03-02 21:28:34 -0600

    adding a dirname path for the database JSON

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,13 +1,13 @@
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
-//getting the note data from the json file:
-const notesData = require("../db/db.json");
-savedNotes = [];
+const path = require("path");
+const notesDatapath  = path.join(__dirname, "../db/db.json");
+const savedNotes = [];
 
 module.exports = function (app) {
   //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
   app.get("/api/notes", function (req, res) {
-    fs.readFile("../Develop/db/db.json", "utf-8", (err, data) => {
+    fs.readFile(notesDatapath, "utf-8", function (err, data) {
       if (err) {
         console.log(err);
       } else {
@@ -22,7 +22,7 @@ module.exports = function (app) {
     newNote.id = uuidv4();
     savedNotes.push(newNote);
 
-    fs.writeFile("../Develop/db/db.json", "utf-8", (err) => {
+    fs.writeFile(notesDatapath, JSON.stringify(savedNotes), "utf-8", (err) => {
       if (err) {
         console.log(err);
       } else {
@@ -32,9 +32,8 @@ module.exports = function (app) {
     res.json(savedNotes);
   });
 
-  //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
   app.delete("/api/notes/:id", function (req, res) {
-    fs.readFile("../Develop/db/db.json", "utf-8", (err, data) => {
+    fs.readFile(notesDatapath, "utf-8", (err, data) => {
       if (err) {
         console.log(err);
       } else {
@@ -42,7 +41,7 @@ module.exports = function (app) {
         const deleted = req.params.id;
         console.log(data);
         notes = newData.filter((data) => data.id != deleted);
-        fs.writeFile("../Develop/db/db.json", "utf-8", (err) => {
+        fs.writeFile(notesDatapath, "utf-8", (err) => {
           if (err) {
             console.log(err);
           }
