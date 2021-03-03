07a6a2f5c60f233e91ea7681ffcfb4c20cb7ae6e
CaroKris <ckhilpisch@gmail.com>
2021-03-03 10:56:56 -0600

    changing varaibles to the correct names

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,18 +5,17 @@ const notesDatapath  = path.join(__dirname, "../db/db.json");
 const savedNotes = [];
 
 module.exports = function (app) {
-  //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
   app.get("/api/notes", function (req, res) {
     fs.readFile(notesDatapath, "utf-8", function (err, data) {
       if (err) {
         console.log(err);
       } else {
-        const savedNotes = JSON.parse(data);
+        let savedNotes = JSON.parse(data);
         res.json(savedNotes);
       }
     });
   });
-  //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
   app.post("/api/notes", function (req, res) {
     const newNote = req.body;
     newNote.id = uuidv4();
@@ -41,12 +40,12 @@ module.exports = function (app) {
         const deleted = req.params.id;
         console.log(data);
         notes = newData.filter((data) => data.id != deleted);
-        fs.writeFile(notesDatapath, "utf-8", (err) => {
+        fs.writeFile(notesDatapath, JSON.stringify(notes), "utf-8", (err) => {
           if (err) {
             console.log(err);
           }
         });
-        res.json(notes);
+        res.json(savedNotes);
       }
     });
   });
